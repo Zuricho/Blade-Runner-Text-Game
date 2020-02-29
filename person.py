7a95c283478aee0b0afdf0e5faa678ae7e19d175
Zuricho <zbztzhz@163.com>
2020-03-01 02:00:46 +0800

    0.7-Add location and time

--- person.py
+++ person.py
@@ -10,11 +10,23 @@ class Person:
         # Job description should refer to readme
         self.status = status
         # status: 0-healthy, 1-first generation infected, 2-seconde generation infected
+
     def getStatus(self):
         if self.status == 0:
             print('you are healthy!')
         else:
             print('you are infected!')
+
+    def location(self,time):
+        # Location: 0-Home; 1-WorkPlace; 2-HomeRestaurant; 3-WorkRestaurant
+        if time%100 in range(0,30)+range(85,100):
+            return 0
+        elif time%100 in range(30,50)+range(55,75):
+            return 1
+        elif time%100 in range(50,55):
+            return 3
+        else:
+            return 2
     
 
 def personInit(number,randomSeed):
